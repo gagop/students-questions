0c14918d9becad33c58c0d0915b0ced485ea4d68
Piotr Gago <gagopiotr@gmail.com>
2020-11-01 14:42:00 +0100

    Added backend and frontend project

---
+++ backend/BackendExample/BackendExample/Controllers/PersonsController.cs
@@ -0,0 +1,26 @@
+﻿using BackendExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BackendExample.Controllers
+{
+    [Route("api/persons")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromForm] PersonRequest request)
+        {
+            FileInfo fi = new FileInfo(request.Avatar.FileName);
+            using (FileStream streamTo = System.IO.File.Create(request.Avatar.FileName))
+            using (Stream streamFrom = request.Avatar.OpenReadStream())
+            {
+                streamFrom.CopyTo(streamTo);
+            }
+
+            return Ok();
+        }
+
+    }
+}
---
+++ backend/BackendExample/BackendExample/Models/PersonRequest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace BackendExample.Models
+{
+    public class PersonRequest
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public IFormFile Avatar { get; set; }
+    }
+}
