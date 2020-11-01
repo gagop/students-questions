0867b79708a847c7b6d3f7ad574e417a1f3714ae
Piotr Gago <gagopiotr@gmail.com>
2020-11-01 15:13:29 +0100

    Added example of sending file from frontend

--- frontend/frontend-example/src/App.js
+++ frontend/frontend-example/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const onSubmitHandler=async (e)=>{
+    e.preventDefault();
+
+    axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+    console.log(e);
+    let formData=new FormData();
+    formData.append('firstName', e.target.firstName.value);
+    formData.append('lastName', e.target.lastName.value);
+    formData.append('avatar', e.target.avatar.files[0]);
+    
+    await axios({
+      method: 'post',
+      url: 'https://localhost:44357/api/persons',
+      data: formData
+    }).then(function(response){
+        console.log('this is response');
+    }).catch(function(error){
+      console.log('this is error!');
+      console.log(error);
+    });
+
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        Przykład formularza
       </header>
+      <main>
+      <form onSubmit={onSubmitHandler}>
+        <label for="firstName">First name:</label>
+        <input type="text" name="firstName" /><br />
+        <label for="lastName">Last name:</label>
+        <input type="text" name="lastName" /><br />
+        <label for="avatar">Avatar</label>
+        <input type="file" name="avatar" />
+        <br /><br />
+        <button type="submit">Dodaj</button>
+      </form>
+      </main>
     </div>
   );
 }
